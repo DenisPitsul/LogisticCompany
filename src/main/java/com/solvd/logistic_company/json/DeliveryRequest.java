c4d473457dde9324023402cc29658e439b17ed13
olga opryshko <40028108+olgaopryshko@users.noreply.github.com>
2020-06-03 22:25:16 +0200

    Add loading a DeliveryRequest from JSON

---
+++ src/main/java/com/solvd/logistic_company/json/DeliveryRequest.java
@@ -0,0 +1,64 @@
+package com.solvd.logistic_company.json;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DeliveryRequest {
+    private static final Logger LOGGER = Logger.getLogger(DeliveryRequest.class);
+
+    @JsonAlias("city_from")
+    private String cityFrom;
+    @JsonAlias("city_to")
+    private String cityTo;
+    private int cargo;
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCityTo() {
+        return cityTo;
+    }
+
+    public void setCityTo(String cityTo) {
+        this.cityTo = cityTo;
+    }
+
+    public String getCityFrom() {
+        return cityFrom;
+    }
+
+    public void setCityFrom(String cityFrom) {
+        this.cityFrom = cityFrom;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryRequest{" +
+                "cityFrom='" + cityFrom + '\'' +
+                ", cityTo='" + cityTo + '\'' +
+                ", cargo=" + cargo +
+                '}';
+    }
+
+    public static DeliveryRequest fromJsonFile(File file) {
+        LOGGER.debug("Loading delivery information from " + file.getAbsolutePath());
+        DeliveryRequest data = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            data = mapper.readValue(file, DeliveryRequest.class);
+
+        } catch (IOException e) {
+            LOGGER.error("Error in loadDeliveryFromFile:", e);
+        }
+        return data;
+    }
+}
